2f8f6c938d8a0f9cf2d78f4739f5af0f72af8e12
sebhajek <seb.hajek@gmail.com>
2025-07-29 15:30:16 +0200

    updated command exec

--- src/main/java/seb_dot_hajek_at_gmail_dot_com/dots/installer/command/CommandExec.java
+++ src/main/java/seb_dot_hajek_at_gmail_dot_com/dots/installer/command/CommandExec.java
@@ -2,6 +2,7 @@ package seb_dot_hajek_at_gmail_dot_com.dots.installer.command;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import seb_dot_hajek_at_gmail_dot_com.dots.installer.config.ConfigManager;
 import seb_dot_hajek_at_gmail_dot_com.dots.shared.Logger;
@@ -19,32 +20,41 @@ public record CommandExec(Command cmd) {
         exec(ConfigManager.cfg().getConfig().dryRun());
 	}
 
+	private static String readOutputStream(InputStream inputStream)
+	  throws              IOException {
+        StringBuilder output = new StringBuilder();
+        try (
+          BufferedReader reader =
+            new BufferedReader(new InputStreamReader(inputStream))
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append(System.lineSeparator());
+            }
+        }
+        return output.toString();
+	}
+
 	private void exec(boolean dryRun)
 	  throws     IOException, InterruptedException, CommandExecutionException {
         if (dryRun) {
             Logger.logger().dryRun(cmd.toString());
         } else {
             Logger.logger().run(cmd.toString());
-            ProcessBuilder builder  = new ProcessBuilder(cmd.cmd());
-            Process        process  = builder.start();
-            int            exitCode = process.waitFor();
+            var builder  = new ProcessBuilder(cmd.cmd());
+            var process  = builder.start();
+            var exitCode = process.waitFor();
 
+            var stdOutput =
+              CommandExec.readOutputStream(process.getInputStream());
             if (exitCode != 0) {
-                StringBuilder errorOutput = new StringBuilder();
-                try (
-                  BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(process.getErrorStream())
-                  )
-                ) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        errorOutput.append(line).append(System.lineSeparator());
-                    }
-                }
-
-                Logger.logger().error(errorOutput.toString());
+                var errorOutput =
+                  CommandExec.readOutputStream(process.getErrorStream());
+                Logger.logger().error(errorOutput);
                 throw new CommandExecutionException(String.format(
-                  "Command failed: %s\nExit code: %d\n", cmd, exitCode
+                  "Command failed: %s\nExit code: %d\n",
+                  cmd.toString(),
+                  exitCode
                 ));
             }
         }
